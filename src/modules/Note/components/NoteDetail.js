8fba494c6bd367c69cb3a1442a6533ef3c08d4b6
Siong Esteban <siong.esteban@gmail.com>
2018-03-15 08:50:37 +0800

    Use NoteDialog in NoteDetail.

--- src/modules/Note/components/NoteDetail.js
+++ src/modules/Note/components/NoteDetail.js
@@ -1,36 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Route, Link } from 'react-router-dom';
 import { push } from 'react-router-redux';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import Hidden from 'material-ui/Hidden';
-import IconButton from 'material-ui/IconButton';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import ArrowBackIcon from 'material-ui-icons/ArrowBack';
-import Dialog, {
-  DialogContent,
-  withMobileDialog,
-} from 'material-ui/Dialog';
 import Typography from 'material-ui/Typography';
 import Divider from 'material-ui/Divider';
 import { withStyles } from 'material-ui/styles';
+
+import NoteDialog from './NoteDialog';
 
 import { updateThemeColor } from '../../Layout/LayoutUtils';
 import formatDate from '../../../utils/formatDate';
 
 const styles = theme => ({
-  appBar: {
-    boxShadow: theme.shadows[1]
-  },
-  menuButton: {
-    marginLeft: -12,
-    color: '#000',
-  },
-  dialogContent: {
-    paddingTop: 20,
-  },
   divider: {
     marginTop: 20,
     marginBottom: 20,
@@ -57,38 +41,16 @@ class NoteDetail extends Component {
   };
 
   render() {
-    const { fullScreen, classes, authUserId } = this.props;
+    const { classes, authUserId } = this.props;
     const note = this.props.note.isPartOfCollab
       ? this.props.note.parentNote
       : this.props.note;
 
     return (
-      <Dialog
-        fullScreen={fullScreen}
-        open
-        onClose={this.handleClose}
-        aria-labelledby="responsive-dialog-title"
-      >
-        <Hidden mdUp>
-          <AppBar
-            className={classes.appBar}
-            style={{ background: note.color || '#fff' }}
-            position="static"
-          >
-            <Toolbar>
-              <IconButton
-                className={classes.menuButton}
-                aria-label="Menu"
-                onClick={this.handleClose}
-              >
-                <ArrowBackIcon />
-              </IconButton>
-            </Toolbar>
-          </AppBar>
-        </Hidden>
-        <DialogContent
-          className={classes.dialogContent}
-          style={{ background: note.color }}
+      <div>
+        <NoteDialog
+          handleClose={this.handleClose}
+          noteColor={this.props.note.color || '#fff'}
         >
           <Typography
             variant="display2"
@@ -170,8 +132,8 @@ class NoteDetail extends Component {
               </div>
             ))
           }
-        </DialogContent>
-      </Dialog>
+        </NoteDialog>
+      </div>
     );
   }
 }
@@ -186,7 +148,6 @@ const mapDispatchToProps = dispatch => bindActionCreators({
 }, dispatch);
 
 NoteDetail.propTypes = propTypes;
-NoteDetail = withMobileDialog()(NoteDetail);
 NoteDetail = withStyles(styles)(NoteDetail);
 NoteDetail = connect(mapStateToProps, mapDispatchToProps)(NoteDetail);
 
